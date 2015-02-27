468596ab29456a635939f0f518c50347eae8c3db
meng004@gmail.com <meng004@gmail.com>
2015-02-27 20:51:14 +0800

    修改实体映射

---
+++ AspNetIdentity2Permission.Mvc/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,39 @@
+﻿using AspNetIdentity2Permission.Mvc.Models;
+using System.ComponentModel;
+
+namespace AspNetIdentity2Permission.Mvc.App_Start
+{
+    [Description("AutoMapper配置")]
+    public class AutoMapperProfile : AutoMapper.Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<ApplicationPermission, PermissionViewModel>();
+            CreateMap<PermissionViewModel, ApplicationPermission>();
+            CreateMap<ApplicationRole, RoleViewModel>();
+            CreateMap<RoleViewModel, ApplicationRole>()
+                .ForMember(
+                            dest => dest.Id, 
+                            sour =>
+                            {
+                                sour.MapFrom(s => s.Id ?? System.Guid.NewGuid().ToString());
+                            });
+            
+            CreateMap<ApplicationUser, EditUserViewModel>();
+            CreateMap<EditUserViewModel, ApplicationUser>();
+            CreateMap<RegisterViewModel, ApplicationUser>();
+        }
+    }
+
+    [Description("AutoMapper匹配")]
+    public class AutoMapperConfig
+    {
+        public static void Configure()
+        {
+            AutoMapper.Mapper.Initialize(cfg =>
+            {
+                cfg.AddProfile<AutoMapperProfile>();
+            });
+        }
+    }
+}
